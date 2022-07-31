46a751c30165f32c574bf1c805ea4f8c30896a6a
Flaviodc97 <flavio.dellacorte1997@gmail.com>
2022-07-31 18:19:07 +0200

    Added Firebase Cloud Firestore for users

--- E-Culture_Tool_A/app/src/main/java/com/example/e_culture_tool_a/RegisterActivity.java
+++ E-Culture_Tool_A/app/src/main/java/com/example/e_culture_tool_a/RegisterActivity.java
@@ -6,22 +6,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText memail,mpassword,mrpassword, mnome,mcognome;
     Button mregisterButton;
     ProgressBar mprogressBar;
     FirebaseAuth fAuth;
+    Switch mruolo;
+    FirebaseFirestore fStore;
+    String user_id;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +44,13 @@ public class RegisterActivity extends AppCompatActivity {
         mpassword = findViewById(R.id.Password);
         mnome = findViewById(R.id.Nome);
         mcognome = findViewById(R.id.Cognome);
+        mrpassword = findViewById(R.id.Rpassword);
         mregisterButton = findViewById(R.id.ButtonRegister);
         mprogressBar = findViewById(R.id.progressBarRegister);
+        mruolo =  findViewById(R.id.Ruolo);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
 
 
@@ -48,6 +65,11 @@ public class RegisterActivity extends AppCompatActivity {
 
             String email = memail.getText().toString().trim();
             String password = mpassword.getText().toString().trim();
+            String nome = mnome.getText().toString().trim();
+            String cognome = mcognome.getText().toString().trim();
+            Boolean ruolo = mruolo.isChecked();
+
+
 
             if(TextUtils.isEmpty(email)){
 
@@ -59,6 +81,16 @@ public class RegisterActivity extends AppCompatActivity {
             if(TextUtils.isEmpty(password)){
 
                 mpassword.setError("Inserisci la Password");
+                return;
+            }
+            if(TextUtils.isEmpty(nome)){
+
+                mnome.setError("Inserisci Il tuo nome");
+                return;
+            }
+            if(TextUtils.isEmpty(cognome)){
+
+                mcognome.setError("Inserisci il tuo cognome");
                 return;
             }
 
@@ -79,6 +111,20 @@ public class RegisterActivity extends AppCompatActivity {
 
                     if(task.isSuccessful()){
                         Toast.makeText(RegisterActivity.this, "Account Creato con successo", Toast.LENGTH_SHORT).show();
+                        user_id = fAuth.getCurrentUser().getUid();
+                        DocumentReference docReference = fStore.collection("utenti").document(user_id);
+                        Map<String,Object> user = new HashMap<>();
+                        user.put("Nome",nome);
+                        user.put("Cognome", cognome);
+                        user.put("E-mail", email);
+                        user.put("Curatore", ruolo);
+                        docReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d("TAG", "Caricato con successo" + user_id);
+                            }
+                        });
+
                         startActivity(new Intent(getApplicationContext(),HomeActivity.class ));
                     }else{
                         Toast.makeText(RegisterActivity.this, "Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
